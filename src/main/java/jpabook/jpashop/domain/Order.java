fa184f2ab2ccf19a848ce6e0738e614ee47cbfe1
gwkim <qkek875@gmail.com>
2023-07-26 17:22:39 +0900

    컬렉션 조회 최적화 V4 - JPA에서 DTO 직접 조회 ~ 진행중

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,23 +9,31 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
+
     @Id @GeneratedValue
     @Column(name = "order_id")
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member; //주문 회원
+
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; //배송정보
+
     private LocalDateTime orderDate; //주문시간
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
@@ -35,10 +43,12 @@ public class Order {
         this.member = member;
         member.getOrders().add(this);
     }
+
     public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
+
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
@@ -66,7 +76,7 @@ public class Order {
             throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
         }
         this.setStatus(OrderStatus.CANCEL);
-        for (OrderItem orderItem: orderItems) {
+        for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
         }
     }
@@ -74,17 +84,14 @@ public class Order {
     //==조회 로직==//
 
     /**
-     *
      * 전체 주문 가격 조회
      */
     public int getTotalPrice() {
         int totalPrice = orderItems.stream()
-                .mapToInt(OrderItem::getTotalPrice)
-                .sum();
+                                   .mapToInt(OrderItem::getTotalPrice)
+                                   .sum();
         return totalPrice;
     }
-
-
 
 
 }
